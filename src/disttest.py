ddcc90dec786fc96ebc5c4d6d99604c335c8dccd
zkorangesun <zhangkai chinaunicom>
2015-04-11 06:33:06 +0800

    add disttest

--- src/disttest.py
+++ src/disttest.py
@@ -6,3 +6,18 @@ name = ['macel','zhangkai','zkorangesun']
 #dist
 d = {"macel":95,"zhangkai":84,"zkorangesun":93}
 print d['macel']
+
+print name[0]
+print d.get('zk')
+if 'zk' in d:
+	print 'y'
+else:
+	print 'n'
+
+
+print abs(-100)
+
+def my_abs(arg):
+	return abs(arg)
+
+print my_abs(-101)
